808341c9009eac5e7f26b89e264ab657708ed247
hyeming <0726minn@naver.com>
2023-09-03 23:45:51 +0900

    로그인 페이지, 회원가입, 주소, 비상연락처 등록, 정보창

---
+++ app/src/main/java/com/example/falldetectionapp/emergency_phone.java
@@ -0,0 +1,14 @@
+package com.example.falldetectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class emergency_phone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_phone);
+    }
+}
