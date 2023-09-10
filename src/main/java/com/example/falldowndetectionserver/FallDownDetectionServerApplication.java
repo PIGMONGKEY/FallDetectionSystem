65811b28a01a3867dd5d59be67c894f7a6f93f14
LeeWonHee <pigmong02020202@gmail.com>
2023-09-10 15:54:19 +0900

    Initial Commit

---
+++ src/main/java/com/example/falldowndetectionserver/FallDownDetectionServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.falldowndetectionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallDownDetectionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FallDownDetectionServerApplication.class, args);
+    }
+
+}
