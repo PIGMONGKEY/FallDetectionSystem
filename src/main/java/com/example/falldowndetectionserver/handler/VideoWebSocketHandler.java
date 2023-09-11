91ae81bb607d1e4fa9282a900992f4ce05a5f34d
LeeWonHee <pigmong02020202@gmail.com>
2023-09-11 17:32:15 +0900

    show-video.html created VideoController.java created Add sessions in VideoWebSocketHandler.java

--- src/main/java/com/example/falldowndetectionserver/handler/VideoWebSocketHandler.java
+++ src/main/java/com/example/falldowndetectionserver/handler/VideoWebSocketHandler.java
@@ -5,13 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.ConcurrentWebSocketSessionDecorator;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 @Component
 @Slf4j
@@ -19,22 +20,47 @@ import java.util.Set;
 public class VideoWebSocketHandler extends TextWebSocketHandler {
     private final HashMap<String, WebSocketSession> sessions = new HashMap<>();
     private String senderSessionID;
-    private List<String> receiverSessionID;
+    private List<String> receiverSessionID = new ArrayList<>();
 
     @Override
     protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
-        log.info(message.getPayload() + "");
+        if (session == sessions.get(senderSessionID)) {
+            for (String key : sessions.keySet()) {
+                WebSocketSession wss = sessions.get(key);
+                try {
+                    wss.sendMessage(message);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        if (session == sessions.get(senderSessionID)) {
+            for (String key : sessions.keySet()) {
+                try {
+                    WebSocketSession wss = sessions.get(key);
+                    wss.sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        if (sessions.isEmpty()){
             session.setBinaryMessageSizeLimit(10000000);
+        if (sessions.isEmpty()){
             sessions.put(session.getId(), session);
             senderSessionID = session.getId();
+            log.info("sender connected : " + senderSessionID);
         } else {
             sessions.put(session.getId(), session);
             receiverSessionID.add(session.getId());
+            log.info("receiver connected : " + session.getId());
         }
     }
 
