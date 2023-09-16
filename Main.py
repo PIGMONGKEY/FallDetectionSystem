2aa2cf2bfa6759cbe9d6e57a06d4c7384fe5280b
LeeWonHee <pigmong02020202@gmail.com>
2023-09-16 16:41:52 +0900

    WebSocket을 통해 서버로 영상(이미지) 보내는 코드 완성 - 테스트 이전

--- Main.py
+++ Main.py
@@ -2,3 +2,22 @@ import cv2
 import asyncio
 import websockets
 
+
+# WebSocket을 통해 서버로 연결하고, Opencv를 이용해 웹캠으로 찍은 영상을 프레임 단위로 전송
+async def send_video_and_position():
+    # localhost:10000/video 로 연결한 WebSocket을 websocket이라는 이름으로 사용
+    async with websockets.connect("ws://localhost:10000/video") as websocket:
+        # 웹캡으로 영상 캡쳐
+        cap = cv2.VideoCapture(0)
+        # 웹캡이 켜져 있는동안 반복
+        while cap.isOpened():
+            # 프레임 단위로 영상 읽음
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # jpg 이미지 형태로 인코딩
+            _, encoded_image = cv2.imencode('jpg', frame)
+            # 이미지를 byte 형식으로 변환
+            encoded_image = encoded_image.tobytes()
+            # 서버로 전송
+            await websocket.send(encoded_image)
