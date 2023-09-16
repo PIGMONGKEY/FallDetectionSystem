80163e6c03c9fdb26a6d60e9ce33ed5e2331b803
LeeWonHee <pigmong02020202@gmail.com>
2023-09-17 01:21:14 +0900

    헤더 사용 안하고, 연결 후 첫 메시지로 연결 정보 전송하는 방식 채택

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
 import cv2
 import asyncio
 import websockets
+import json
 
 
-HEADERS = {
+CONNECTION_INFO = json.dumps({
     "camera_id": "cam01",
     "identifier": "sender"
-}
+})
 
 
 # WebSocket을 통해 서버로 연결하고, Opencv를 이용해 웹캠으로 찍은 영상을 프레임 단위로 전송
 async def send_video_and_position():
     # localhost:10000/video 로 연결한 WebSocket을 websocket이라는 이름으로 사용
-    async with websockets.connect("ws://localhost:10000/video", extra_headers=HEADERS) as websocket:
+    async with websockets.connect("ws://localhost:10000/video") as websocket:
+        # 연결되면 연결 정보를 TextMessage로 보냄
+        await websocket.send(CONNECTION_INFO)
         # 웹캡으로 영상 캡쳐
         cap = cv2.VideoCapture(0)
         # 웹캡이 켜져 있는동안 반복
