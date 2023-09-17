d0d11cdefe37cefdb85ec6782c68a7410543086c
JJIONY-Y <wldnjs2159@naver.com>
2023-09-17 20:35:53 +0900

    아직 작성중입니다.. 공주는 보지 말것!!!

--- app/src/main/java/com/example/falldetectionapp/MainActivity.java
+++ app/src/main/java/com/example/falldetectionapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.falldetectionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("로딩화면");
+
+        Button btnmain =(Button) findViewById(R.id.button5);
+
+        btnmain.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, address.class);
+                startActivity(intent);
+            }
+        });
     }
 
 //    코드 작성!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- app/src/main/java/com/example/falldetectionapp/info.java
+++ app/src/main/java/com/example/falldetectionapp/info.java
@@ -3,6 +3,9 @@ package com.example.falldetectionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class info extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        setTitle("개인정보");
+
+        EditText Name = (EditText) findViewById(R.id.editTextTextPassword4);
+        EditText Phone = (EditText) findViewById(R.id.editTextTextPassword5);
+        EditText CfNum = (EditText) findViewById(R.id.editTextTextPassword3);
+
+        Button CfBtn = (Button) findViewById(R.id.button3);
+        Button CfOkBtn = (Button) findViewById(R.id.button10);
+        Button btnNext = (Button) findViewById(R.id.button9);
+
+        // 인증받기 버튼 수정
+        CfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CfBtn.setText("재전송");
+
+            }
+        });
+
+        CfOkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
