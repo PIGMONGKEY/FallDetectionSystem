2bcaeffdcba3a451ef7f108c55932e26ea220db4
LeeWonHee <pigmong02020202@gmail.com>
2023-09-20 12:42:50 +0900

    UserMappper 주석 추가

--- src/main/java/com/example/falldowndetectionserver/mapper/UserMapper.java
+++ src/main/java/com/example/falldowndetectionserver/mapper/UserMapper.java
@@ -8,10 +8,28 @@ import org.springframework.stereotype.Component;
 @Mapper
 @Component
 public interface UserMapper {
+    /**
+     *
+     * @param userVO uno, regdate, updatedate 제외하고, 모두 넘겨줘야 한다.
+     */
     public void insert(UserVO userVO);
+
+    /**
+     *
+     * @param uno primary key인 uno만 넘겨주면 됨
+     * @return UserVO형태로 리턴함
+     */
     public UserVO select(int uno);
+
+    /**
+     *
+     * @param uno primary key인 uno만 넘겨주면 됨
+     */
     public void delete(int uno);
 
-//    이름, 비밀번호, 전화번호, 주소, 업데이트 시간
+    /**
+     *
+     * @param userVO userName, userPassword, userAddress, userPhone 이 네개는 꼭 필요함
+     */
     public void update(UserVO userVO);
 }
