bcc7f36d6c0041cf32aec56c76732ee0ce490606
LeeWonHee <pigmong02020202@gmail.com>
2023-09-20 11:58:25 +0900

    Create UserMapper / insert() test done

--- src/test/java/com/example/falldowndetectionserver/mapper/MapperTests.java
+++ src/test/java/com/example/falldowndetectionserver/mapper/MapperTests.java
@@ -1,6 +1,7 @@
 package com.example.falldowndetectionserver.mapper;
 
 import com.example.falldowndetectionserver.domain.TestDTO;
+import com.example.falldowndetectionserver.domain.UserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class MapperTests {
     @Autowired
     private TestMapper testMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     public void selectTest() {
@@ -19,9 +22,13 @@ public class MapperTests {
 
     @Test
     public void insertTest() {
-        TestDTO testDTO = new TestDTO();
-        testDTO.setTestId("test");
-        testDTO.setTestName("test");
-        testMapper.insert(testDTO);
+        UserVO userVO = new UserVO();
+        userVO.setUserName("이원희");
+        userVO.setUserPassword("Dnjsgml0202?");
+        userVO.setUserAddress("경기도 의정부시 호원동 현대아이파크 201-1201");
+        userVO.setUserPhone("010-5729-2701");
+        userVO.setCameraId("cam02");
+
+        userMapper.insert(userVO);
     }
 }
