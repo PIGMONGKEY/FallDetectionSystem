69e8a551601af0618fd4b4ca894dbc43d97cae10
LeeWonHee <pigmong02020202@gmail.com>
2023-09-25 18:06:23 +0900

    페이지 이동 구현 - 단순 이동만 될 수 있도록 버튼 리스너 하나씩만 달아놓음

--- app/src/main/java/com/example/falldetectionapp/LoginActivity.java
+++ app/src/main/java/com/example/falldetectionapp/LoginActivity.java
@@ -2,13 +2,50 @@ package com.example.falldetectionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.falldetectionapp.register.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button toRegisterButton, toHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        init();
+    }
+
+    private void init() {
+        setView();
+        setListener();
+    }
+
+    private void setView() {
+        toRegisterButton = findViewById(R.id.registerButton_login);
+        toHomeButton = findViewById(R.id.loginButton_login);
+    }
+
+    private void setListener() {
+        toRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/falldetectionapp/register/NokPhoneActivity.java
+++ app/src/main/java/com/example/falldetectionapp/register/NokPhoneActivity.java
@@ -2,15 +2,43 @@ package com.example.falldetectionapp.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.falldetectionapp.GuideActivity;
+import com.example.falldetectionapp.HomeActivity;
 import com.example.falldetectionapp.R;
 
 public class NokPhoneActivity extends AppCompatActivity {
+
+    private Button registerDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_phone);
+
+        init();
+    }
+
+    private void init() {
+        setView();
+        setListener();
+    }
+
+    private void setView() {
+        registerDone = findViewById(R.id.finishButton_register);
+    }
+
+    private void setListener() {
+        registerDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NokPhoneActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
