d7fb8674188126de64113129aac514b617dd93b0
JJIONY-Y <wldnjs2159@naver.com>
2023-09-26 12:21:47 +0900

    guide 수정 중입니다 완성 후 다시 올려드릴게요

--- app/src/main/java/com/example/falldetectionapp/GuideActivity.java
+++ app/src/main/java/com/example/falldetectionapp/GuideActivity.java
@@ -3,19 +3,32 @@ package com.example.falldetectionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 가이드를 보여주는 창입니다.
  * activity_guide.xml과 연결됩니다.
  */
 public class GuideActivity extends AppCompatActivity {
+
+    private ImageButton backButton;
+    private ImageSlider guideSlider;
+
+    private List<SlideModel> slideModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +38,26 @@ public class GuideActivity extends AppCompatActivity {
         init();
     }
 
-//    초기 설정을 넣어주세요
+    //    초기 설정을 넣어주세요
     private void init() {
+        setTitle("가이드");
         setView();
         setListener();
     }
 
     private void setView() {
+        backButton = findViewById(R.id.imageButton4);
+        guideSlider = findViewById(R.id.slider);
+
+        slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.guide1));
+        slideModels.add(new SlideModel(R.drawable.guide2));
+        slideModels.add(new SlideModel(R.drawable.guide3));
 
     }
 
-//    리스너는 여기 모아주세요
+    //    리스너는 여기 모아주세요
     private void setListener() {
 
     }
