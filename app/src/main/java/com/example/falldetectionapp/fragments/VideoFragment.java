6f6fc7670cdef22ce8ea21a84f06c9fd6e2bc8d6
LeeWonHee <pigmong02020202@gmail.com>
2023-09-26 11:14:26 +0900

    video연결 브랜치 작업 시작

--- app/src/main/java/com/example/falldetectionapp/fragments/VideoFragment.java
+++ app/src/main/java/com/example/falldetectionapp/fragments/VideoFragment.java
@@ -1,14 +1,30 @@
 package com.example.falldetectionapp.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.falldetectionapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 /**
  * HomeActivity의 FrameLayout에 들어가는 VideoFragment입니다.
@@ -20,6 +36,10 @@ import com.example.falldetectionapp.R;
  * Activity와는 다르게 독자적으로 존재할 수 없는 화면이고, 화면 안에 있는 화면이다보니, 로직을 짜는 방식이 조금 다를껍니다.
  */
 public class VideoFragment extends Fragment {
+
+    private WebSocket socket;
+    private final JSONObject CONNECTION_INFO = new JSONObject();
+    private ImageView videoImageView;
 
 //    빈 생성자가 있어야 합니다. 삭제하면 안됩니다.
     public VideoFragment() { }
@@ -34,5 +54,82 @@ public class VideoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        init();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        socket.close(1000, "close");
+    }
+
+    private void init() {
+        setView();
+
+        try {
+            videoReceive();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setView() {
+        videoImageView = getActivity().findViewById(R.id.videoImageView);
+    }
+
+    private void videoReceive() throws InterruptedException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("ws://10.0.2.2:10000/video").build();
+
+        socket = client.newWebSocket(request, new WebSocketListener() {
+            @Override
+            public void onClosed(@NonNull WebSocket webSocket, int code, @NonNull String reason) {
+                Log.d("video", "closed");
+            }
+
+            @Override
+            public void onClosing(@NonNull WebSocket webSocket, int code, @NonNull String reason) {
+                Log.d("video", "closing");
+            }
+
+            @Override
+            public void onFailure(@NonNull WebSocket webSocket, @NonNull Throwable t, @Nullable Response response) {
+                Log.d("video", "failure : " + t.getCause() + " / " + t.getMessage() + " / " + t);
+            }
+
+            @Override
+            public void onMessage(@NonNull WebSocket webSocket, @NonNull String text) {
+                Log.d("video", "get message : " + text);
+            }
+
+            @Override
+            public void onMessage(@NonNull WebSocket webSocket, @NonNull ByteString bytes) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showImage(BitmapFactory.decodeByteArray(bytes.toByteArray(), 0, bytes.toByteArray().length));
+                    }
+                });
+            }
+
+            @Override
+            public void onOpen(@NonNull WebSocket webSocket, @NonNull Response response) {
+                Log.d("video", "connected");
+                try {
+                    CONNECTION_INFO.put("identifier", "receiver");
+                    CONNECTION_INFO.put("camera_id", "cam01");
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                webSocket.send(CONNECTION_INFO.toString());
+            }
+        });
+    }
+
+
+    private void showImage(Bitmap bitmap) {
+        videoImageView.setImageBitmap(bitmap);
     }
 }
