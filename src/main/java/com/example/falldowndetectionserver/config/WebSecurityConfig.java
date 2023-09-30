3f83cf0a1220c6c1ba3a6a76d4ba834023f09ca3
LeeWonHee <pigmong02020202@gmail.com>
2023-10-01 03:03:13 +0900

    Service 제외 생성

---
+++ src/main/java/com/example/falldowndetectionserver/config/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.example.falldowndetectionserver.config;
+
+import com.example.falldowndetectionserver.jwt.JwtAccessDeniedHandler;
+import com.example.falldowndetectionserver.jwt.JwtAuthenticationEntryPoint;
+import com.example.falldowndetectionserver.jwt.JwtFilter;
+import com.example.falldowndetectionserver.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    // PasswordEncoder는 BCryptPasswordEncoder를 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다.
+                .requestMatchers("/user/").permitAll() // 로그인 api
+                .requestMatchers("/api/signup").permitAll() // 회원가입 api
+                .requestMatchers(PathRequest.toH2Console()).permitAll()// h2-console, favicon.ico 요청 인증 무시
+                .requestMatchers("/favicon.ico").permitAll()
+                .anyRequest().authenticated() // 그 외 인증 없이 접근X
+
+                .and()
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling()
+                .accessDeniedHandler(new JwtAccessDeniedHandler())
+                .authenticationEntryPoint(new JwtAuthenticationEntryPoint());
+
+        return httpSecurity.build();
+    }
+
+}
---
+++ src/main/java/com/example/falldowndetectionserver/domain/LoginDTO.java
@@ -0,0 +1,9 @@
+package com.example.falldowndetectionserver.domain;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String camearId;
+    private String password;
+}
