819716a9700e592abd88c7379c37d4974109d609
LeeWonHee <pigmong02020202@gmail.com>
2023-10-04 19:34:03 +0900

    NotificationTestActivity 추가 AndroidManifest.xml에서 메인페이지로 열리도록 변경

---
+++ app/src/main/java/com/example/falldetectionapp/notification/NotificationTestActivity.java
@@ -0,0 +1,16 @@
+package com.example.falldetectionapp.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.falldetectionapp.R;
+
+public class NotificationTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_test);
+    }
+}
