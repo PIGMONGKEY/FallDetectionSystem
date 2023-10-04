bc30b14edd4a1f69554af55c3eca8747cf1f7b90
LeeWonHee <pigmong02020202@gmail.com>
2023-10-05 01:45:54 +0900

    알림도 뜨고, 잠겨있을 때 열리기도 하지만, 원하는 화면으로 열리지 않음 - 어떻게 해야 열리는지 차근차근 알아볼 예정

--- app/src/main/java/com/example/falldetectionapp/notification/NotificationTestActivity.java
+++ app/src/main/java/com/example/falldetectionapp/notification/NotificationTestActivity.java
@@ -1,11 +1,15 @@
 package com.example.falldetectionapp.notification;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.Manifest;
+import android.app.AlarmManager;
+import android.app.KeyguardManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -14,18 +18,36 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.falldetectionapp.R;
 
 public class NotificationTestActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_test);
 
+        button = findViewById(R.id.button_noti);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Thread.sleep(4000);
+                    turnScreenOnAndKeyguardOff();
+                    sendNotification();
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
         createNotificationChannel();
-        sendNotification();
     }
 
     private void sendNotification() {
@@ -37,21 +59,48 @@ public class NotificationTestActivity extends AppCompatActivity {
                 .setSmallIcon(R.drawable.ic_home)
                 .setContentTitle("textTitle")
                 .setContentText("textContent")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentIntent(fullScreenPendingIntent)
+                .setFullScreenIntent(fullScreenPendingIntent, true);
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             Log.d("Notification", "NO PERMISSION");
             return;
         }
+
         notificationManagerCompat.notify(1, builder.build());
+    }
+
+    private void turnScreenOnAndKeyguardOff() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                    WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+        } else {
+            getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+//                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+            );
+        }
+
+//        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            keyguardManager.requestDismissKeyguard(this, null);
+//        }
+    }
+
+    private void fullScreenNotiWithAlert() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+//        alarmManager.setExactAndAllowWhileIdle(
+//                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+
+//        );
     }
 
     private void createNotificationChannel() {
