0aea697346e30b7f8f4bccf6230fae0fc73fdf4e
LeeWonHee <pigmong02020202@gmail.com>
2023-10-04 20:39:31 +0900

    기본 알림 띄우기 성공 - 작업표시줄을 내려야 보임

---
+++ app/src/main/java/com/example/falldetectionapp/notification/ImportantActivity.java
@@ -0,0 +1,16 @@
+package com.example.falldetectionapp.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.falldetectionapp.R;
+
+public class ImportantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_important);
+    }
+}
--- app/src/main/java/com/example/falldetectionapp/notification/NotificationTestActivity.java
+++ app/src/main/java/com/example/falldetectionapp/notification/NotificationTestActivity.java
@@ -1,8 +1,19 @@
 package com.example.falldetectionapp.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.falldetectionapp.R;
 
@@ -12,5 +23,43 @@ public class NotificationTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_test);
+
+        createNotificationChannel();
+        sendNotification();
+    }
+
+    private void sendNotification() {
+        Intent fullScreenIntent = new Intent(this, ImportantActivity.class);
+        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(this, 0,
+                fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "test")
+                .setSmallIcon(R.drawable.ic_home)
+                .setContentTitle("textTitle")
+                .setContentText("textContent")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d("Notification", "NO PERMISSION");
+            return;
+        }
+        notificationManagerCompat.notify(1, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("test", "name", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("description");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
