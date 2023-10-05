e68336fa4a10cc557ca9f40d72501055ebe2d78c
LeeWonHee <pigmong02020202@gmail.com>
2023-10-05 22:15:09 +0900

    Firebase setting and Create MyFirebaseMessageService.java

---
+++ app/src/main/java/com/example/falldetectionapp/services/MyFirebaseMessageService.java
@@ -0,0 +1,7 @@
+package com.example.falldetectionapp.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessageService extends FirebaseMessagingService {
+
+}
