e05470d90a2b744df5e00dfc315c89703ba9223c
LeeWonHee <pigmong02020202@gmail.com>
2023-10-05 16:26:23 +0900

    잠금화면 깨워서 Activity 띄우기 가능함 확인

--- app/src/main/java/com/example/falldetectionapp/notification/NotificationTestActivity.java
+++ app/src/main/java/com/example/falldetectionapp/notification/NotificationTestActivity.java
@@ -3,18 +3,7 @@ package com.example.falldetectionapp.notification;
 import static java.lang.Thread.sleep;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import android.Manifest;
-import android.app.AlarmManager;
 import android.app.KeyguardManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,52 +15,25 @@ import com.example.falldetectionapp.R;
 
 public class NotificationTestActivity extends AppCompatActivity {
 
-    private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_test);
 
-        button = findViewById(R.id.button_noti);
-        button.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button_noti).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    Thread.sleep(4000);
+                    sleep(6000);
                     turnScreenOnAndKeyguardOff();
-                    sendNotification();
-                } catch (Exception e) {
-
+                    Log.d("TEST", "Screen Turn ON ");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
-
-        createNotificationChannel();
     }
 
-    private void sendNotification() {
-        Intent fullScreenIntent = new Intent(this, ImportantActivity.class);
-        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(this, 0,
-                fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "test")
-                .setSmallIcon(R.drawable.ic_home)
-                .setContentTitle("textTitle")
-                .setContentText("textContent")
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setContentIntent(fullScreenPendingIntent)
-                .setFullScreenIntent(fullScreenPendingIntent, true);
-
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            Log.d("Notification", "NO PERMISSION");
-            return;
-        }
-
-        notificationManagerCompat.notify(1, builder.build());
-    }
 
     private void turnScreenOnAndKeyguardOff() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
@@ -82,33 +44,16 @@ public class NotificationTestActivity extends AppCompatActivity {
         } else {
             getWindow().addFlags(
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-//                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
                 WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
             );
         }
 
-//        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            keyguardManager.requestDismissKeyguard(this, null);
-//        }
-    }
-
-    private void fullScreenNotiWithAlert() {
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-//        alarmManager.setExactAndAllowWhileIdle(
-//                AlarmManager.ELAPSED_REALTIME_WAKEUP,
-
-//        );
-    }
-
-    private void createNotificationChannel() {
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("test", "name", NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription("description");
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+            keyguardManager.requestDismissKeyguard(this, null);
         }
     }
 }
