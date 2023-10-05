7f99cd3d25c1cb4104bdca823696f3d30253e3fa
LeeWonHee <pigmong02020202@gmail.com>
2023-10-05 22:18:38 +0900

    get firebase refreshed token

--- app/src/main/java/com/example/falldetectionapp/services/MyFirebaseMessageService.java
+++ app/src/main/java/com/example/falldetectionapp/services/MyFirebaseMessageService.java
@@ -1,7 +1,15 @@
 package com.example.falldetectionapp.services;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MyFirebaseMessageService extends FirebaseMessagingService {
-
+    @Override
+    public void onNewToken(@NonNull String token) {
+        Log.d("FCM Log", "token : " + token);
+//        f9dD-3EDQlq_7q2GYMztCF:APA91bFNolsE_zMQkSKeSggoAdlvhTRifUNczr4DUV5Bt6Rsi-Kzn92d9tXI5hDBf72k_yqeJ_UC9rZCa8mtgT3z2YdCRKXHPuLc4mMBBTS4rkdxkLvQ2PLRuBaO49AETga9-lhfSUsW
+    }
 }
