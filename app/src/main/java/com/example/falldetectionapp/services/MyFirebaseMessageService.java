c78a09e484b34101d59d10d9d692699a744b9086
LeeWonHee <pigmong02020202@gmail.com>
2023-10-06 18:17:58 +0900

    push 알림이 왔을 때, 전체화면으로 알림을 띄우고, 잠자는 화면을 띄우는 작업 시작

--- app/src/main/java/com/example/falldetectionapp/services/MyFirebaseMessageService.java
+++ app/src/main/java/com/example/falldetectionapp/services/MyFirebaseMessageService.java
@@ -5,11 +5,18 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessageService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         Log.d("FCM Log", "token : " + token);
 //        f9dD-3EDQlq_7q2GYMztCF:APA91bFNolsE_zMQkSKeSggoAdlvhTRifUNczr4DUV5Bt6Rsi-Kzn92d9tXI5hDBf72k_yqeJ_UC9rZCa8mtgT3z2YdCRKXHPuLc4mMBBTS4rkdxkLvQ2PLRuBaO49AETga9-lhfSUsW
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        Log.d("FCM Log", "Message received : " + message);
+        super.onMessageReceived(message);
     }
 }
