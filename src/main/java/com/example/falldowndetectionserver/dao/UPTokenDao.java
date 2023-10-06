0843be21596da497947cb123d59d14d9d529f105
LeeWonHee <pigmong02020202@gmail.com>
2023-10-06 19:15:31 +0900

    Create UPTokenDao

---
+++ src/main/java/com/example/falldowndetectionserver/dao/UPTokenDao.java
@@ -0,0 +1,36 @@
+package com.example.falldowndetectionserver.dao;
+
+import com.example.falldowndetectionserver.domain.vo.UPTokenVO;
+import com.example.falldowndetectionserver.mapper.UPTokenMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@Component
+@RequiredArgsConstructor
+public class UPTokenDao {
+    private final UPTokenMapper uPTokenMapper;
+
+    public int insert(UPTokenVO uPTokenVO) {
+        try {
+            return uPTokenMapper.insert(uPTokenVO);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public Optional<String> select(String cameraId) {
+        return uPTokenMapper.select(cameraId);
+    }
+
+    public int delete(String cameraId) {
+        try {
+            return uPTokenMapper.delete(cameraId);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+}
--- src/main/java/com/example/falldowndetectionserver/mapper/UPTokenMapper.java
+++ src/main/java/com/example/falldowndetectionserver/mapper/UPTokenMapper.java
@@ -4,10 +4,12 @@ import com.example.falldowndetectionserver.domain.vo.UPTokenVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Mapper
 @Component
 public interface UPTokenMapper {
     public int insert(UPTokenVO upTokenVO);
-    public String select(String cameraId);
+    public Optional<String> select(String cameraId);
     public int delete(String cameraId);
 }
