43016ec9636f278e8731c398e6dccc6e84396f4f
LeeWonHee <pigmong02020202@gmail.com>
2023-10-06 21:05:55 +0900

    d

--- src/test/java/com/example/falldowndetectionserver/mapper/UPTokenMapperTests.java
+++ src/test/java/com/example/falldowndetectionserver/mapper/UPTokenMapperTests.java
@@ -17,10 +17,10 @@ public class UPTokenMapperTests {
         uPTokenMapper.insert(UPTokenVO.builder().cameraId("cam01").token("testToken").build());
     }
 
-    @Test
-    public void selectTest() {
-        log.info(uPTokenMapper.select("cam01"));
-    }
+//    @Test
+//    public void selectTest() {
+//        log.info(uPTokenMapper.select("cam01"));
+//    }
 
     @Test
     public void deleteTest() {
