ff3b547a3cd4eda82732385ab2fdac8bf89a106e
LeeWonHee <pigmong02020202@gmail.com>
2023-10-08 16:54:12 +0900

    update access key

--- app/src/main/java/com/example/falldetectionapp/services/MyFirebaseMessageService.java
+++ app/src/main/java/com/example/falldetectionapp/services/MyFirebaseMessageService.java
@@ -11,7 +11,6 @@ public class MyFirebaseMessageService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         Log.d("FCM Log", "token : " + token);
-//        f9dD-3EDQlq_7q2GYMztCF:APA91bFNolsE_zMQkSKeSggoAdlvhTRifUNczr4DUV5Bt6Rsi-Kzn92d9tXI5hDBf72k_yqeJ_UC9rZCa8mtgT3z2YdCRKXHPuLc4mMBBTS4rkdxkLvQ2PLRuBaO49AETga9-lhfSUsW
     }
 
     @Override
