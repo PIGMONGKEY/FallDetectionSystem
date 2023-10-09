8da28d618b9eb527494b72ca78c0fd8efc4eda56
LeeWonHee <pigmong02020202@gmail.com>
2023-10-10 03:22:53 +0900

    화면도 잘 켜지고, 알림도 잘 오길래, 잠금화면 위에 Activity 띄우려 했는데 안 됨

--- app/src/main/java/com/example/falldetectionapp/notification/ImportantActivity.java
+++ app/src/main/java/com/example/falldetectionapp/notification/ImportantActivity.java
@@ -2,7 +2,12 @@ package com.example.falldetectionapp.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 
 import com.example.falldetectionapp.R;
 
@@ -12,5 +17,30 @@ public class ImportantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_important);
+
+        turnScreenOnAndKeyguardOff();
+    }
+
+    private void turnScreenOnAndKeyguardOff() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                    WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+        } else {
+            getWindow().addFlags(
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                            WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+            );
+        }
+
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.d("TEST", "Keyguard disable");
+            keyguardManager.requestDismissKeyguard(this, null);
+        }
     }
 }
