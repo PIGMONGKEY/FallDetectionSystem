aab05b6839d7e670657a6d51f98c17124582e5e5
LeeWonHee <pigmong02020202@gmail.com>
2023-10-14 19:32:06 +0900

    모든 데이터 다 보내게 수정함

--- Main.py
+++ Main.py
@@ -56,6 +56,8 @@ async def get_position_and_send(frame, position_socket):
     outputs = movenet(pose_image)['output_0']
 
     for keypoint in outputs.numpy()[0][0]:
+        for key in keypoint:
+            key = float(key)
         if keypoint[2] <= 0.4:
             continue
         if keypoint[0] <= min_y:
@@ -68,6 +70,25 @@ async def get_position_and_send(frame, position_socket):
             max_x = keypoint[1]
 
     temp = {
+        "position": [
+            [1*outputs.numpy()[0][0][0][0], 1*outputs.numpy()[0][0][0][1], 1*outputs.numpy()[0][0][0][2]],
+            [1*outputs.numpy()[0][0][1][0], 1*outputs.numpy()[0][0][1][1], 1*outputs.numpy()[0][0][1][2]],
+            [1*outputs.numpy()[0][0][2][0], 1*outputs.numpy()[0][0][2][1], 1*outputs.numpy()[0][0][2][2]],
+            [1*outputs.numpy()[0][0][3][0], 1*outputs.numpy()[0][0][3][1], 1*outputs.numpy()[0][0][3][2]],
+            [1*outputs.numpy()[0][0][4][0], 1*outputs.numpy()[0][0][4][1], 1*outputs.numpy()[0][0][4][2]],
+            [1*outputs.numpy()[0][0][5][0], 1*outputs.numpy()[0][0][5][1], 1*outputs.numpy()[0][0][5][2]],
+            [1*outputs.numpy()[0][0][6][0], 1*outputs.numpy()[0][0][6][1], 1*outputs.numpy()[0][0][6][2]],
+            [1*outputs.numpy()[0][0][7][0], 1*outputs.numpy()[0][0][7][1], 1*outputs.numpy()[0][0][7][2]],
+            [1*outputs.numpy()[0][0][8][0], 1*outputs.numpy()[0][0][8][1], 1*outputs.numpy()[0][0][8][2]],
+            [1*outputs.numpy()[0][0][9][0], 1*outputs.numpy()[0][0][9][1], 1*outputs.numpy()[0][0][9][2]],
+            [1*outputs.numpy()[0][0][10][0], 1*outputs.numpy()[0][0][10][1], 1*outputs.numpy()[0][0][10][2]],
+            [1*outputs.numpy()[0][0][11][0], 1*outputs.numpy()[0][0][11][1], 1*outputs.numpy()[0][0][11][2]],
+            [1*outputs.numpy()[0][0][12][0], 1*outputs.numpy()[0][0][12][1], 1*outputs.numpy()[0][0][12][2]],
+            [1*outputs.numpy()[0][0][13][0], 1*outputs.numpy()[0][0][13][1], 1*outputs.numpy()[0][0][13][2]],
+            [1*outputs.numpy()[0][0][14][0], 1*outputs.numpy()[0][0][14][1], 1*outputs.numpy()[0][0][14][2]],
+            [1*outputs.numpy()[0][0][15][0], 1*outputs.numpy()[0][0][15][1], 1*outputs.numpy()[0][0][15][2]],
+            [1*outputs.numpy()[0][0][16][0], 1*outputs.numpy()[0][0][16][1], 1*outputs.numpy()[0][0][16][2]]
+        ],
         "min_x": int(round(min_x, 2) * 100) if min_x != 99999 else -1,
         "max_x": int(round(max_x, 2) * 100) if max_x != 0 else -2,
         "min_y": int(round(min_y, 2) * 100) if min_y != 99999 else -1,
