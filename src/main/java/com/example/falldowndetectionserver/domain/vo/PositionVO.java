fb33d9c6aa55bb7a4655c8cd35273446d0641bfc
LeeWonHee <pigmong02020202@gmail.com>
2023-10-14 19:55:25 +0900

    Position 전체 정보 받아옴

--- src/main/java/com/example/falldowndetectionserver/domain/vo/PositionVO.java
+++ src/main/java/com/example/falldowndetectionserver/domain/vo/PositionVO.java
@@ -6,6 +6,26 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 public class PositionVO {
+    public static final String[] keypoint = {   "nose",
+                                                "left eye",
+                                                "right eye",
+                                                "left ear",
+                                                "right ear",
+                                                "left shoulder",
+                                                "right shoulder",
+                                                "left elbow",
+                                                "right elbow",
+                                                "left wrist",
+                                                "right wrist",
+                                                "left hip",
+                                                "right hip",
+                                                "left knee",
+                                                "right knee",
+                                                "left ankle",
+                                                "right ankle"
+                                            };
+
+    private double[][] position;
     private int min_x;
     private int min_y;
     private int max_x;
