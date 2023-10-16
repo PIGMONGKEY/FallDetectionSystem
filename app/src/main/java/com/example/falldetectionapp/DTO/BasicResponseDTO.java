3fea7e1129db2ad7d6d5a3fb4e19acf26f2f1811
LeeWonHee <pigmong02020202@gmail.com>
2023-10-17 00:49:56 +0900

    BasicResponseDTO 생성 UserInfoDTO 변경

---
+++ app/src/main/java/com/example/falldetectionapp/DTO/BasicResponseDTO.java
@@ -0,0 +1,38 @@
+package com.example.falldetectionapp.DTO;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class BasicResponseDTO<T> {
+    @SerializedName("code")
+    @Expose
+    private Integer code;
+
+    @SerializedName("httpStatus")
+    @Expose
+    private String httpStatus;
+
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    @SerializedName("data")
+    @Expose
+    private T data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getHttpStatus() {
+        return httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
--- app/src/main/java/com/example/falldetectionapp/DTO/UserInfoDTO.java
+++ app/src/main/java/com/example/falldetectionapp/DTO/UserInfoDTO.java
@@ -10,49 +10,25 @@ import java.util.List;
  * 사용자 정보를 받아올 때 사용하는 DTO 입니다.
  */
 public class UserInfoDTO implements Serializable {
-    @SerializedName("requestSuccess")
-    @Expose
-    private String requestSuccess;
 
     @SerializedName("cameraId")
     @Expose
     private String cameraId;
-
     @SerializedName("userName")
     @Expose
     private String userName;
-
     @SerializedName("userPassword")
     @Expose
     private String userPassword;
-
     @SerializedName("userPhone")
     @Expose
     private String userPhone;
-
     @SerializedName("userAddress")
     @Expose
     private String userAddress;
-
-    @SerializedName("regdate")
-    @Expose
-    private String regdate;
-
-    @SerializedName("updatedate")
-    @Expose
-    private String updatedate;
-
-    @SerializedName("userRole")
-    @Expose
-    private String userRole;
-
     @SerializedName("nokPhones")
     @Expose
     private List<String> nokPhones;
-
-    public String getRequestSuccess() {
-        return requestSuccess;
-    }
 
     public String getCameraId() {
         return cameraId;
