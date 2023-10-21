48def7fc00aa81bcdf612f41dbbf9d457b93669a
LeeWonHee <pigmong02020202@gmail.com>
2023-10-21 15:15:14 +0900

    update userVo

--- src/main/java/com/example/falldowndetectionserver/domain/vo/UserVO.java
+++ src/main/java/com/example/falldowndetectionserver/domain/vo/UserVO.java
@@ -7,12 +7,15 @@ import org.springframework.stereotype.Component;
 @Data
 @Builder
 public class UserVO {
-    private String cameraId;
-    private String userName;
-    private String userPassword;
-    private String userPhone;
-    private String userAddress;
-    private String regdate;
-    private String updatedate;
-    private String userRole;
+    private final String cameraId;
+    private final String userPassword;
+    private final String userName;
+    private final String userAge;
+    private final String userGender;
+    private final String userBloodType;
+    private final String userPhone;
+    private final String userAddress;
+    private final String userRole;
+    private final String regdate;
+    private final String updatedate;
 }
