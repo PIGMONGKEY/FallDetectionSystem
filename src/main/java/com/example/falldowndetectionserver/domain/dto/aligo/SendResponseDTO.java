dcd33d9cc721fe50b1f8a924f375a4f951158a56
LeeWonHee <pigmong02020202@gmail.com>
2023-10-28 20:18:01 +0900

    Aligo DTO 작성

--- src/main/java/com/example/falldowndetectionserver/domain/dto/aligo/SendResponseDTO.java
+++ src/main/java/com/example/falldowndetectionserver/domain/dto/aligo/SendResponseDTO.java
@@ -1,4 +1,15 @@
 package com.example.falldowndetectionserver.domain.dto.aligo;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class SendResponseDTO {
+    private final Integer result_code;
+    private final String message;
+    private final Integer msg_id;
+    private final Integer success_cnt;
+    private final Integer error_cnt;
+    private final String msg_type;
 }
