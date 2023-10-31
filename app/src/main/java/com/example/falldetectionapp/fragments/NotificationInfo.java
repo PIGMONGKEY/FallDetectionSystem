45ae9c5ad197490aaa02fd788b04875da67d47a7
LeeWonHee <pigmong02020202@gmail.com>
2023-11-01 01:06:07 +0900

    공지사항 자세히 보기 틀 제작 완료

---
+++ app/src/main/java/com/example/falldetectionapp/fragments/NotificationInfo.java
@@ -0,0 +1,46 @@
+package com.example.falldetectionapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.falldetectionapp.R;
+
+public class NotificationInfo extends Fragment {
+
+    public NotificationInfo() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification_info, container, false);
+    }
+
+    private void init(View view) {
+
+    }
+
+    private void setView(View view) {
+
+    }
+
+    private void requestNotificationInfo() {
+
+    }
+
+    private void showNotificationContent() {
+
+    }
+}
