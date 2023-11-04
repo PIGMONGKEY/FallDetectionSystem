064405451a7ca4187323eb5fcc9dbfa4382aee45
LeeWonHee <pigmong02020202@gmail.com>
2023-11-04 16:18:35 +0900

    Create FCM Notification Activity Create FCM Notification Service

---
+++ app/src/main/java/com/example/falldowndetectionnokapp/notification/EmergencyNotificationActivity.java
@@ -0,0 +1,16 @@
+package com.example.falldowndetectionnokapp.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.falldowndetectionnokapp.R;
+
+public class EmergencyNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_notification);
+    }
+}
