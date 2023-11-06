ad715c076ff51cd864225309ff70649370a252bf
LeeWonHee <pigmong02020202@gmail.com>
2023-11-07 00:57:27 +0900

    자동로그인 추가

--- app/src/main/java/com/example/falldetectionapp/fragments/MyPageFragment.java
+++ app/src/main/java/com/example/falldetectionapp/fragments/MyPageFragment.java
@@ -1,8 +1,10 @@
 package com.example.falldetectionapp.fragments;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -298,6 +300,27 @@ public class MyPageFragment extends Fragment {
                         .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+
+                                // 자동 로그인 정보 삭제
+                                SharedPreferences sp = getContext().getSharedPreferences("autoLogin", Activity.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sp.edit();
+
+                                // 혹시 값이 이미 있다면 삭제
+                                if (sp.contains("auto")) {
+                                    editor.remove("auto");
+                                    editor.commit();
+                                }
+
+                                if (sp.contains("cameraId")) {
+                                    editor.remove("cameraId");
+                                    editor.commit();
+                                }
+
+                                if (sp.contains("password")) {
+                                    editor.remove("password");
+                                    editor.commit();
+                                }
+
                                 Intent intent = new Intent(getActivity(), StartActivity.class);
                                 startActivity(intent);
                             }
